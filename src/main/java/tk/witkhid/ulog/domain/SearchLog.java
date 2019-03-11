cdc0a8e42008c1fa6087738b94d1d122c4af284e
Justin <anomie7777@gmail.com>
2019-03-11 14:36:25 +0900

    [impl] searchLog 명세

--- src/main/java/tk/witkhid/ulog/domain/SearchLog.java
+++ src/main/java/tk/witkhid/ulog/domain/SearchLog.java
@@ -1,4 +1,23 @@
 package tk.witkhid.ulog.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
 public class SearchLog {
+    private Long userId;
+    private String Category;
+    private String region;
+    private String timestamp;
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
 }
