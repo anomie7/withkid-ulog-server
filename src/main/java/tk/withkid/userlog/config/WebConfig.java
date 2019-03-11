77e35792e1e49398de61be18b358927037f79718
Justin <anomie7777@gmail.com>
2019-03-11 20:34:58 +0900

    [config] cors 설정 추가

---
+++ src/main/java/tk/withkid/userlog/config/WebConfig.java
@@ -0,0 +1,20 @@
+package tk.withkid.userlog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+		.allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS")
+		.allowedOrigins("*")
+		.allowedHeaders("*")
+		.exposedHeaders(HttpHeaders.AUTHORIZATION);
+	}
+}
