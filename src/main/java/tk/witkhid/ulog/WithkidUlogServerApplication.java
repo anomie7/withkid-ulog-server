c6d8ac16b25c84c9454720d96e7a5b8295fc3670
Justin <anomie7777@gmail.com>
2019-03-11 13:13:20 +0900

    init

---
+++ src/main/java/tk/witkhid/ulog/WithkidUlogServerApplication.java
@@ -0,0 +1,13 @@
+package tk.witkhid.ulog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithkidUlogServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithkidUlogServerApplication.class, args);
+	}
+
+}
