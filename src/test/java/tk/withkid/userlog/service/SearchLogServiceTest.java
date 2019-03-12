0df9629beda93ed874e21510cd5e3d673b909b14
Justin <anomie7777@gmail.com>
2019-03-12 20:56:57 +0900

    [impl] SearchLogService의 getUserId에 대한 테스트 코드 작성

--- src/test/java/tk/withkid/userlog/service/SearchLogServiceTest.java
+++ src/test/java/tk/withkid/userlog/service/SearchLogServiceTest.java
@@ -1,5 +1,6 @@
 package tk.withkid.userlog.service;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,13 +17,21 @@ import static org.junit.Assert.*;
 public class SearchLogServiceTest {
     @Autowired
     private SearchLogService searchLogService;
+    private String accessTkn = "eyJ0eXAiOiJKV1QiLCJyZWdEYXRlIjoxNTUyMzkwOTQ3NTE5LCJ0eXBlIjoiYWNjZXNzLXRva2VuIiwiYWxnIjoiSFMyNTYifQ.eyJ1c2VyLWlkIjo1LCJleHAiOjE1ODQwMTY5NDcsImVtYWlsIjoiZGVwcm9tZWV0QHRyYWN0NC5jb20ifQ.J2DqpOYDEXfM-d49X2-G3qSdROZLKWmEgUxQOZMXp6w";
 
     @Test
     public void saveSearchLog() throws ExecutionException, InterruptedException {
         SearchLog searchLog = SearchLog.builder().userId(1L).category("Mu").region("서울").build();
 
-        String res = searchLogService.saveSearchLog(searchLog);
+        String res = searchLogService.saveSearchLog(accessTkn, searchLog);
 
         assertNotNull(res);
+    }
+
+    @Test
+    public void getUserId() {
+        Long userId = searchLogService.getUserId(accessTkn);
+
+        assertTrue(userId.equals(5L));
     }
 }
