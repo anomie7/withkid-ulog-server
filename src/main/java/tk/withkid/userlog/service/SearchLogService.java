862cb522501ba6c3192404546676072716c3dbb2
Justin <anomie7777@gmail.com>
2019-03-12 20:55:09 +0900

    [impl] getUserId 추가 기능: auth-server에 accessToken을 보내서 userId를 조회하는 기능, 토큰이 무효하면 userId 받아오지 못하고 클라이언트에 errorResponse 반환

--- src/main/java/tk/withkid/userlog/service/SearchLogService.java
+++ src/main/java/tk/withkid/userlog/service/SearchLogService.java
@@ -1,9 +1,13 @@
 package tk.withkid.userlog.service;
 
-import com.google.cloud.firestore.DocumentReference;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 import tk.withkid.userlog.domain.SearchLog;
 import tk.withkid.userlog.repository.FIrestoreRepository;
 
@@ -23,16 +27,26 @@ public class SearchLogService {
         this.fIrestoreRepository = fIrestoreRepository;
     }
 
-    public String saveSearchLog(SearchLog searchLog) throws ExecutionException, InterruptedException {
+    public String saveSearchLog(String accessToken, SearchLog searchLog) throws ExecutionException, InterruptedException {
         LocalDateTime now = LocalDateTime.now(ZoneOffset.UTC);
         String random = String.valueOf(new Random().nextInt(100000));
         String docId = now + "." + random;
 
         String timestamp = now.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
-        searchLog.setTimestamp(timestamp);
+        Long userId = this.getUserId(accessToken);
+        searchLog.setStorableLog(userId, timestamp);
 
         String updateTIme = fIrestoreRepository.saveSearchLog(docId, searchLog);
         log.info("search user log update time: {}", updateTIme);
         return updateTIme;
+    }
+
+    public Long getUserId(String accessToken) {
+        RestTemplate restTemplate = new RestTemplate();
+        UriComponents uri = UriComponentsBuilder.fromUriString("http://localhost").path("/userId").port(8082).build();
+
+        RequestEntity<Void> req = RequestEntity.get(uri.toUri()).header("Authorization", accessToken).build();
+        ResponseEntity<Long> res = restTemplate.exchange(req, Long.class);
+        return res.getBody();
     }
 }
