a4b6d4fe407232969d8aa35538e52d08a1830d6d
Justin <anomie7777@gmail.com>
2019-03-12 20:53:24 +0900

    [modify]  로그 메시지 추가

--- src/main/java/tk/withkid/userlog/controller/ExceptionController.java
+++ src/main/java/tk/withkid/userlog/controller/ExceptionController.java
@@ -1,5 +1,6 @@
 package tk.withkid.userlog.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,14 +8,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpStatusCodeException;
 
+@Slf4j
 @ControllerAdvice
 @RestController
 public class ExceptionController {
 
     @ExceptionHandler(HttpStatusCodeException.class)
-    public ResponseEntity<ErrorResponse> requestFailHandler() {
+    public ResponseEntity<ErrorResponse> requestFailHandler(Exception e) {
         ErrorResponse body = ErrorResponse.builder().name(HttpStatusCodeException.class.getSimpleName())
                 .msg("요청이 실패했습니다.").status(HttpStatus.NOT_FOUND).build();
+        log.error("restTemplate의 요청이 실패함: msg : {}", e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
     }
 }
