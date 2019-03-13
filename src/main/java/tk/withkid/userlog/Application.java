5c30c6bdda40affe6e27f8b583b8af0564004687
Justin <anomie7777@gmail.com>
2019-03-13 17:30:44 +0900

    [dependency add] cirecuitBreaker인 hystrix 추가

--- src/main/java/tk/withkid/userlog/Application.java
+++ src/main/java/tk/withkid/userlog/Application.java
@@ -2,7 +2,9 @@ package tk.withkid.userlog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
+@EnableCircuitBreaker
 @SpringBootApplication()
 public class Application {
 
