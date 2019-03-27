75e23b603f2a9d5ef30e5f4ac2005c5deaa27505
Justin <anomie7777@gmail.com>
2019-03-28 01:10:59 +0900

    [Impl] ResourceService 추가

--- src/main/java/tk/withkid/userlog/service/ResourceService.java
+++ src/main/java/tk/withkid/userlog/service/ResourceService.java
@@ -1,13 +1,17 @@
 package tk.withkid.userlog.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -23,7 +27,14 @@ public class ResourceService {
         this.env = env;
     }
 
-    public ResponseEntity<?> getEventsOf(List<Long> eventIds){
-        return null;
+    public JsonNode getEventsOf(List<Long> eventIds) throws IOException {
+        String pathVar = eventIds.toString().replace("[", "").replace("]", "");
+        UriComponents uriComponents = UriComponentsBuilder.fromUriString("http://localhost").path("/event/".concat(pathVar)).port(8081).build();
+        String uriString = uriComponents.toUriString();
+        String res = restTemplate.getForObject(uriString, String.class);
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(res);
+        return jsonNode;
     }
 }
