307e8ba4de8693f6a2a1a8db4c5f0b238f4a10d7
Justin <anomie7777@gmail.com>
2019-04-06 17:14:57 +0900

    [Impl] transaction 적용해서 유저의 로그와 이벤트의 조회수를 저장하는 로직 작성

--- src/main/java/tk/withkid/userlog/repository/EventLogRepository.java
+++ src/main/java/tk/withkid/userlog/repository/EventLogRepository.java
@@ -1,18 +1,21 @@
 package tk.withkid.userlog.repository;
 
 import com.google.api.core.ApiFuture;
+import com.google.cloud.Timestamp;
 import com.google.cloud.firestore.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import tk.withkid.userlog.domain.EventLog;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 @Repository
 public class EventLogRepository {
     private Firestore firestore;
-    private final String eventLogCollection = "event-ulog";
+    private final String userLogCollection = "event-ulog";
 
     @Autowired
 
@@ -21,15 +24,30 @@ public class EventLogRepository {
     }
 
     public String saveEventLog(String docId, EventLog eventLog) throws ExecutionException, InterruptedException {
-        DocumentReference docRef = firestore.collection(eventLogCollection).document(docId);
-        ApiFuture<WriteResult> set = docRef.set(eventLog);
-
-        String updateTIme = String.valueOf(set.get().getUpdateTime());
-        return updateTIme;
+        DocumentReference ulogRef = firestore.collection(userLogCollection).document(docId);
+        DocumentReference countRef = firestore.collection("event-count").document(String.valueOf(eventLog.getEventId()));
+        ApiFuture<Timestamp> transaction = firestore.runTransaction(new Transaction.Function<Timestamp>() {
+            @Override
+            public Timestamp updateCallback(Transaction transaction) throws Exception {
+                DocumentSnapshot snapshot = transaction.get(countRef).get();
+                if (snapshot.exists()) {
+                    Long oldCount = snapshot.getLong("count");
+                    transaction.update(countRef, "count", oldCount + 1);
+                } else {
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("eventId", eventLog.getEventId());
+                    map.put("count", 1L);
+                    transaction.set(countRef, map);
+                }
+                transaction.set(ulogRef, eventLog);
+                return snapshot.getUpdateTime();
+            }
+        });
+        return transaction.get().toString();
     }
 
     public List<EventLog> findRecentEventLog(Long userId) throws ExecutionException, InterruptedException {
-        CollectionReference eventUlog = firestore.collection(eventLogCollection);
+        CollectionReference eventUlog = firestore.collection(userLogCollection);
         Query query = eventUlog.whereEqualTo("userId", userId)
                 .orderBy("timestamp", Query.Direction.DESCENDING)
                 .limit(30);
